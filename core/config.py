11370444762a651d09cbcee8fe0c3af6a8ab5728
Edwin Abot <eabot@trustar.co>
2020-09-04 18:35:09 -0300

    more permissionning and attaching Rule to lambda

--- core/config.py
+++ core/config.py
@@ -10,6 +10,8 @@ try:
     DYNAMO_ENDPOINT = os.getenv("DYNAMO_ENDPOINT", None)
     FIXTURE_PATH = os.getenv("FIXTURE_PATH", "tests/fixture.json")
     JOB_CREATION_FUNCTION = os.getenv("JOB_CREATION_FUNCTION", None)
+    JOB_CREATION_FUNCTION_ARN = os.getenv("JOB_CREATION_FUNCTION_ARN", None)
+    EXTRACTION_JOB_QUEUE = os.getenv("EXTRACTION_JOB_QUEUE", None)
     JOB_TABLE = os.getenv("JOB_TABLE", None)
     USER_CONF_TABLE = os.getenv("USER_CONF_TABLE", None)
     TEMPLATES_TABLE = os.getenv("TEMPLATES_TABLE", None)
--- lambdas/cron_scheduler.py
+++ lambdas/cron_scheduler.py
@@ -1,5 +1,7 @@
 import json
+
 import boto3
+import botocore
 
 from enum import Enum
 
@@ -23,20 +25,21 @@ def lambda_handler(event, context):
     logger.debug(context)
     # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/cw-example-events.html
     events = boto3.client("events")
+    lambdas = boto3.client("lambda")
     handlers = {
         DynamoEvent.INSERT.value: handle_insert,
         DynamoEvent.MODIFY.value: handle_modify,
         DynamoEvent.REMOVE.value: handle_remove,
     }
     for record in event.get("Records", []):
-        handlers[record["eventName"]](record, events)
+        handlers[record["eventName"]](record, events, lambdas)
 
 
 def get_rule_name(job_id: str):
     return f"etl_assembly_job_{job_id}"
 
 
-def handle_insert(record, client):
+def handle_insert(record, client, lambdas):
     job = Job.get(record["dynamodb"]["Keys"]["id"]["S"])  # type: Job
     rule_response = client.put_rule(
         Name=get_rule_name(job.id),
@@ -45,37 +48,50 @@ def handle_insert(record, client):
         Description=f"ETL-Assembly Rule Job ID: {job.id} {job.description}",
     )
     logger.debug(rule_response)
-    exploded_arn = rule_response["RuleArn"].split(":")
-    job_creation_function_arn = (
-        f"arn:aws:lambda:{exploded_arn[3]}:{exploded_arn[4]}"
-        f":function:{config.JOB_CREATION_FUNCTION}"
-    )
     target_response = client.put_targets(
         Rule=get_rule_name(job.id),
         Targets=[
             {
-                "Arn": job_creation_function_arn,
+                "Arn": config.JOB_CREATION_FUNCTION_ARN,
                 "Id": config.JOB_CREATION_FUNCTION,
                 "Input": json.dumps({"config_id": job.id}),
             }
         ],
     )
     logger.debug(target_response)
+    try:
+        lambdas.add_permission(
+            FunctionName=config.JOB_CREATION_FUNCTION,
+            StatementId=get_rule_name(job.id),
+            Action="lambda:InvokeFunction",
+            Principal="events.amazonaws.com",
+            SourceArn=rule_response["RuleArn"],
+        )
+        logger.info(f"InvokeFunction action granted to Rule {get_rule_name(job.id)}")
+    except botocore.exceptions.ClientError as error:
+        # https://boto3.amazonaws.com/v1/documentation/
+        # api/latest/guide/error-handling.html
+        # #parsing-error-responses-and-catching-exceptions-from-aws-services
+        logger.debug(error)
+        logger.info(
+            f"InvokeFunction action already granted to Rule {get_rule_name(job.id)}"
+        )
+
     logger.info(f"EventBridge Rule Put for Job {job.id}: {job.name}")
 
 
-def handle_modify(record, client):
-    handle_insert(record, client)
+def handle_modify(record, client, lambdas):
+    handle_insert(record, client, lambdas)
 
 
-def handle_remove(record, client):
+def handle_remove(record, client, lambdas):
     conf_id = record["dynamodb"]["Keys"]["id"]["S"]
     logger.debug(f"Removing cronjob for deleted configuration {conf_id}")
+    lambdas.remove_permission(
+        FunctionName=config.JOB_CREATION_FUNCTION, StatementId=get_rule_name(conf_id)
+    )
     targets_response = client.remove_targets(
-        Rule=get_rule_name(conf_id),
-        Ids=[
-            config.JOB_CREATION_FUNCTION,
-        ],
+        Rule=get_rule_name(conf_id), Ids=[config.JOB_CREATION_FUNCTION,],
     )
     logger.debug(targets_response)
     client.delete_rule(Name=get_rule_name(conf_id))
