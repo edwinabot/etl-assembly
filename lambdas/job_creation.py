57ce3bc73b54d0abed21f63c34a6eb153ad9e8cb
Edwin Abot <eabot@trustar.co>
2020-09-07 12:30:15 -0300

    variable name change and cleanup

--- lambdas/job_creation.py
+++ lambdas/job_creation.py
@@ -7,10 +7,8 @@ logger = get_logger(__name__)
 
 
 def lambda_handler(event, context):
-    # Build a job object
     logger.debug(event)
     logger.debug(context)
-    # {'config_id': '6c7e83fd-9c22-45ff-829d-59c887917c6f'}
     conf_id = event["config_id"]
-    transformation_queue = SqsQueue(queue_url=config.EXTRACT_JOBS_QUEUE)
-    job_creation_stage(conf_id, transformation_queue)
+    extraction_queue = SqsQueue(queue_url=config.EXTRACT_JOBS_QUEUE)
+    job_creation_stage(conf_id, extraction_queue)
