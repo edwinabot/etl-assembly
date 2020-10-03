4a6ac011d50cb41847bcec8e8339fa550ea83f01
Edwin Abot <eabot@trustar.co>
2020-10-03 10:05:16 -0300

    specific region

--- devops/deploy_production.sh
+++ devops/deploy_production.sh
@@ -1,6 +1,7 @@
 sam deploy \
     --s3-bucket sourcebucket-trustar-etl-assembly \
     --s3-prefix production \
+    --region us-east-1 \
     --parameter-overrides \
         'EnvironmentName="Production" SecurityGroupId="sg-0718d5cd3a6119947" SubnetIdAz0="subnet-0f2f8aad02e17bf4d" SubnetIdAz1="subnet-032c0f59550930451"' \
     --stack-name ETL-Assembly-Production \
