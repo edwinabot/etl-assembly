d1117f17215a8bc7065d193d2e7f2e883d5fc045
Edwin Abot <eabot@trustar.co>
2020-10-03 09:14:33 -0300

    foo

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,1 +1,8 @@
-sam deploy --s3-prefix staging --parameter-overrides 'ParameterKey=EnvironmentName,ParameterValue=Staging' --stack-name ETL-Assembly-Staging --tags 'application=etl-assembly,stage=Staging' --profile eco_ic
+sam deploy \
+    --s3-bucket sourcebucket-trustar-etl-assembly \
+    --s3-prefix staging \
+    --parameter-overrides \
+        'ParameterKey=EnvironmentName,ParameterValue=Staging ParameterKey=SecurityGroupIds,ParameterValue="[sg-05b6b0659f231d6ce]" ParameterKey=SubnetIds,ParameterValue="[subnet-0307e55c069b91394, subnet-052358644cb97d659]"' \
+    --stack-name ETL-Assembly-Staging \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --profile eco_admin
